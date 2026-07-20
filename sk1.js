6777853d53eb6dd04da56900572bdd08ebc16a3f
Moraxqt <gilchristwidjaja@gmail.com>
2026-07-20 15:14:17 +0800

    sk_1

--- sk1.js
+++ sk1.js
@@ -7,8 +7,16 @@ for (let i = 0; i < hargaBarang.length; i++) {
   totalBelanja += hargaBarang[i];
 }
 
+let status = "";
+
 if (totalBelanja > 30000) {
   console.log("Selamat! Anda mendapatkan diskon.");
+  status = "Mendapat Diskon";
 } else {
-  console.log("Total belanja belum mencapai batas diskon."); 
+  console.log("Total belanja belum mencapai batas diskon.");
+  status = "Tidak Mendapat Diskon";
 }
+
+console.log(""); 
+
+console.log(`Nama : ${nama}`, `Umur : ${umur}`, `Total Belanja : ${totalBelanja}`, `Status : ${status}`);
